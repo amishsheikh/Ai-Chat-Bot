133d8e84addcd6a0331d26f8bf20c24f574f912d
amishseikh <sheikhamish20@gmail.com>
2025-09-06 23:37:51 +0530

    first commit

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,17 +1,15 @@
-const { GoogleGenAI } = require("@google/genai")
+const { GoogleGenAI } = require("@google/genai");
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function generateContent(prompt) {
+  const result = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: prompt,
+  });
 
-    const result = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
-        contents: prompt
-    })
-
-    return result.text
-
+  return result.text;
 }
 
 module.exports = { generateContent };
